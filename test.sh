c79d68b48ea832e80cdb5425e1a2c3e3828204e5
JanBlatterCPNV <48351896+JanBlatterCPNV@users.noreply.github.com>
2022-10-14 11:35:49 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
-echo "Hello World"
+#!/bin/.sh
 
+IP ="10.10.10.114"
+netmask ="255.255.255.0"
 
 
 sudo hostnamectl set-hostname SRV-LIN2-02
-sudo ifconfig eth0 10.10.10.114 netmask 255.255.255.0
+sudo ifconfig eth0 $IP netmask $NETMASK
 sudo route add default gw 10.10.10.2 eth0
 sudo nano /etc/resolv.conf
 nameserver 10.10.10.11
