3926f51d3883c02a20c4f222ae112052f6fecb09
JanBlatterCPNV <48351896+JanBlatterCPNV@users.noreply.github.com>
2022-10-14 11:04:46 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,3 +5,6 @@ echo "Hello World"
 sudo hostnamectl set-hostname SRV-LIN2-02
 sudo ifconfig eth0 10.10.10.114 netmask 255.255.255.0
 sudo route add default gw 10.10.10.2 eth0
+sudo nano /etc/resolv.conf
+nameserver 10.10.10.11
+nameserver 10.10.10.2
